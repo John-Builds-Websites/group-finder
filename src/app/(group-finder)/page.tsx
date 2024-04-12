b766b93cb331d4e1c911eacb8d11da0a5beb862d
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-12 18:19:03 +0100

    Refactor group-finder page component

--- src/app/(group-finder)/page.tsx
+++ src/app/(group-finder)/page.tsx
@@ -1,17 +1,17 @@
 "use server";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default async function HomePage() {
-  return (
-    <main className="container mx-auto">
-      <div className="flex flex-col gap-4">
-        <h1>Home</h1>
-        {/* <Button variant={"default"} asChild> */}
-          <Link href={"/groups/"}>See Groups</Link>
-        {/* </Button> */}
-      </div>
-    </main>
-  );
+	return (
+		<main className="container mx-auto">
+			<div className="flex flex-col gap-4">
+				<h1 className="text-3xl font-bold underline text-blue-700">Home</h1>
+				<Button variant="default" asChild>
+					<Link href="/groups/">See Groups</Link>
+				</Button>
+			</div>
+		</main>
+	);
 }
