3e354721c88acdb732c8aa62966e2b21e43682e3
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-12 18:32:07 +0100

    Update button styling and link in Home page

--- src/app/(group-finder)/page.tsx
+++ src/app/(group-finder)/page.tsx
@@ -1,15 +1,15 @@
 "use server";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default async function HomePage() {
 	return (
 		<main className="container mx-auto">
 			<div className="flex flex-col gap-4">
-				<h1 className="text-3xl font-bold underline text-blue-700">Home</h1>
+				<h1 className="text-3xl font-bold text-blue-700">Home</h1>
 				<Button variant="default" asChild>
-					<Link href="/groups/">See Groups</Link>
+					<Link href="/groups/" className={buttonVariants()}>See Groups</Link>
 				</Button>
 			</div>
 		</main>
