3fcb9440df8d73a103f649ca46ff3daa4b356e4b
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-12 12:56:26 +0100

    init commit

---
+++ src/app/(group-finder)/layout.tsx
@@ -0,0 +1,33 @@
+// import Header from "@/components/header";
+// import { Toaster } from "@/components/ui/sonner";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+// import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Group Finder",
+  description: "Find YOUR local group",
+  creator: "John Dennehy",
+  keywords: ["group", "finder", "local"],
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    // <ClerkProvider>
+      <html lang="en">
+        {/* <body className={GeistSans.className}> */}
+        <body>
+          {/* <Header title="Test" /> */}
+          {children}
+          {/* <Toaster /> */}
+        </body>
+      </html>
+    // </ClerkProvider>
+  );
+}
