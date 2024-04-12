25c4117f5ec74c64f4effee0d41ecdf2af2aaddd
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-12 17:35:41 +0100

    Add Geist package and update layout styles

--- src/app/(group-finder)/layout.tsx
+++ src/app/(group-finder)/layout.tsx
@@ -1,33 +1,38 @@
 // import Header from "@/components/header";
 // import { Toaster } from "@/components/ui/sonner";
 // import { ClerkProvider } from "@clerk/nextjs";
-// import { GeistSans } from "geist/font/sans";
+import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: "Group Finder",
-  description: "Find YOUR local group",
-  creator: "John Dennehy",
-  keywords: ["group", "finder", "local"],
+	title: "Group Finder",
+	description: "Find YOUR local group",
+	creator: "John Dennehy",
+	keywords: ["group", "finder", "local", "community",	"social", "johnbuildswebsites"],
 };
 
 type RootLayoutProps = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    // <ClerkProvider>
-      <html lang="en">
-        {/* <body className={GeistSans.className}> */}
-        <body>
-          {/* <Header title="Test" /> */}
-          {children}
-          {/* <Toaster /> */}
-        </body>
-      </html>
-    // </ClerkProvider>
-  );
+	return (
+		// <ClerkProvider>
+		<html lang="en">
+			<body className={
+				cn(
+					"min-h-screen relative bg-background font-sans antialiased",
+					GeistSans.className,
+				  )
+			}>
+				{/* <Header title="Test" /> */}
+				{children}
+				{/* <Toaster /> */}
+			</body>
+		</html>
+		// </ClerkProvider>
+	);
 }
