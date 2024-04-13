5bb148ce0a2ac0ce92b96cf09cb53e5bb2eaca7f
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-13 16:41:35 +0100

    Refactor Container component

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -2,14 +2,19 @@ import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 type ContainerProps = {
-    className?: string;
-    children: ReactNode;
+	className?: string;
+	children?: ReactNode;
 };
 
 export default function Container({ className, children }: ContainerProps) {
-    return (
-        <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px20 ",className) }>
-        {children}
-        </div>
-    );
+	return (
+		<div
+			className={cn(
+				"mx-auto w-full max-w-screen-xl px-2.5 md:px20 ",
+				className,
+			)}
+		>
+			{children}
+		</div>
+	);
 }
