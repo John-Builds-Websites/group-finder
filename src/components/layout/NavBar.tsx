b27dd35fe72530cff7d0d74f72596390650d1fda
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-14 23:49:06 +0100

    Add NavBar component to layout

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,19 @@
+import Container from "./Container";
+
+export default function NavBar() {
+	return (
+		<div className="bg-white sticky z-50 t-0 inset-x-0 h-16 ">
+			<header className="relative bg-white border-b border-gray-100">
+				<Container>
+          <div className="">
+
+					<div className="flex items-center justify-between h-16">
+            {/* TODO Mobile Nav */}
+test
+        </div>
+          </div>
+				</Container>
+			</header>
+		</div>
+	);
+}
