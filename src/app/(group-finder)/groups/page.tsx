5ad6df8f3bc0836220e4c0f51c4329ce0ac7d668
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-15 13:54:08 +0100

    Refactor GroupsPage component

--- src/app/(group-finder)/groups/page.tsx
+++ src/app/(group-finder)/groups/page.tsx
@@ -1,52 +1,47 @@
-'use server'
+"use server";
 
-import Container from '@/components/layout/Container'
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
+import Container from "@/components/layout/Container";
+import configPromise from "@/cms/payload.config";
+import { getPayload } from "payload";
+import type { User } from "@payload-types";
 
-export default async function GroupsPage () {
-  const payload = await getPayload({
-    config: configPromise,
-  })
+export default async function GroupsPage() {
+	const payload = await getPayload({
+		config: configPromise,
+	});
 
-  const {docs: groups} = await payload.find({
-    collection: 'groups',
-  })
+	const { docs: groups } = await payload.find({
+		collection: "groups",
+	});
 
-  return (
-    <Container>
-      <h1>Groups</h1>
-      <ul>
-        {groups.map((group) => (
-          <li key={group.id}>
-            <h2>{group.title}</h2>
-            <p>{group.description}</p>
-            {/* <h3>Followers</h3>
-            <ul>
-              {group.followers.map((follower) => (
-                <li key={follower._id}>{follower.email}</li>
-              ))}
-            </ul> */}
+	return (
+		<Container>
+			{/* <pre>{JSON.stringify(groups, null, 2)}</pre> */}
+			<h1>Groups</h1>
+			<ul>
+				{groups.map((group) => (
+					<li key={group.id}>
+						<h2>{group.title}</h2>
+						<p>{group.description}</p>
 
-            {
-              group.moderators.length > 0 && (
-                <><h3>Moderators</h3><ul>
-                  {group.moderators.map((moderator) => (
-                    <li key={moderator.id}>{moderator.email}</li>
-                  ))}
-                </ul></>
-              )
-            }
+            {group.followers?.map((follower) => {
+              if (typeof follower === "number") return;
+              return <p key={follower.id}>{follower.email}</p>;
+            })}
 
-            <h3>Suitable for</h3>
-            <ul>
-              {group['attendee-types'].map((attendeeType) => (
-                <li key={attendeeType.id}>{attendeeType.name}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </Container>
-  )
+						{group.moderators?.map((moderator) => {
+							if (typeof moderator === "number") return;
+							return <p key={moderator.id}>{moderator.email}</p>;
+						})}
+
+            {group["attendee-types"]?.map((attendeeType) => {
+              if (typeof attendeeType === "number") return;
+              return <p key={attendeeType.id}>{attendeeType.name}</p>;
+            })}
+
+					</li>
+				))}
+			</ul>
+		</Container>
+	);
 }
