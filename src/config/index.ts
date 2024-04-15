f8b9ea67b7176b4d6dd0189188e88813bdfaac22
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-15 19:31:50 +0100

    Add USER_ROLES constant to config file

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const USER_ROLES = [
+  { label: 'User', value: 'user' },
+  { label: 'Moderator', value: 'moderator' },
+  { label: 'Admin', value: 'admin' },
+  { label: 'Super Admin', value: 'super_admin' },
+] as const
