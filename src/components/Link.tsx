bdfd9803b2340cb62a4ce698d29dfb149b5e1196
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-25 15:53:04 +0100

    Update Link component to include autoCapitalize, referrerPolicy, and target attributes

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -10,6 +10,9 @@ export default function Link({ children, ...props }: LinkProps) {
   return (
     <NextLink
       className="relative text-blue-600 after:bg-blue-600 after:absolute after:h-px after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
+      autoCapitalize="true"
+      referrerPolicy="no-referrer"
+      target="_blank"
       {...props}
     >
       {children}
