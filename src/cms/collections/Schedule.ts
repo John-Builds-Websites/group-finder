85714e9c1ec09adc36eb535059b4fa27fd1b33ff
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-27 18:07:20 +0100

    Add Schedule collection configuration

---
+++ src/cms/collections/Schedule.ts
@@ -0,0 +1,99 @@
+import { CollectionConfig } from "payload/types";
+
+export const Schedule: CollectionConfig = {
+  slug: "schedule",
+  admin: {
+    useAsTitle: "row",
+    // defaultColumns: ["name", "date", "time", "location"],
+    // hidden: (user) => {
+    //   return !user || user.authed.role !== "admin";
+    //   },
+
+  }, fields: [
+ 
+    {
+      type: "row",
+      fields: [
+        {
+          name: "weekdays",
+          label: "Weekdays",
+          type: "select",
+          options: [
+            { label: "Mon", value: "mon" },
+            { label: "Tue", value: "tue" },
+            { label: "Wed", value: "wed" },
+            { label: "Thu", value: "thu" },
+            { label: "Fri", value: "fri" },
+            { label: "Sat", value: "sat" },
+            { label: "Sun", value: "sun" },
+          ],
+          hasMany: true,
+          admin: {
+            width: "30%",
+            style: {
+              minWidth: "120px",
+              maxWidth: "400px",
+            },
+          },
+        },
+        {
+          name: "startTime",
+          label: "Start Time",
+          type: "date",
+          required: true,
+          admin: {
+            placeholder: "10:00",
+            width: "140px",
+            style: {
+              minWidth: "120px",
+              maxWidth: "140px",
+            },
+            date: {
+              displayFormat: "h:mm",
+              pickerAppearance: "timeOnly",
+              timeIntervals: 15,
+              timeFormat: "HH:mm",
+            }
+          },
+        },
+        {
+          name: "endTime",
+          label: "End Time",
+          type: "date",
+          required: true,
+          admin: {
+            width: "140px",
+            style: {
+              minWidth: "120px",
+              maxWidth: "140px",
+            },
+            date: {
+              displayFormat: "h:mm",
+              pickerAppearance: "timeOnly",
+              timeIntervals: 15,
+              timeFormat: "HH:mm",
+            }
+          },
+        },
+        {
+          name: "additionalInfo",
+          label: "Additional Information",
+          type: "textarea",
+          maxLength: 100,
+          required: false,
+        },
+        {
+          name: "location",
+          label: "Location (if different)",
+          type: "relationship",
+          relationTo: "locations",
+          required: false,
+        },
+      ],
+    },
+  ],
+  timestamps: true,
+
+
+
+};
