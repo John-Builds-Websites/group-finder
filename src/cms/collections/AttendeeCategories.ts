8eda04cadb3a9739512f19ac3af3766c700a4152
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-27 18:06:51 +0100

    Add new color and shade options for AttendeeCategories

--- src/cms/collections/AttendeeCategories.ts
+++ src/cms/collections/AttendeeCategories.ts
@@ -28,6 +28,53 @@ export const AttendeeCategories: CollectionConfig = {
 			type: "textarea",
 			// required: true,
 		},
+		{
+			type: "row",
+			fields: [
+				{
+					// tailwind colors
+					name: "color",
+					type: "select",
+					options: [
+						{ label: "Red", value: "red" },
+						{ label: "Orange", value: "orange" },
+						{ label: "Yellow", value: "yellow" },
+						{ label: "Lime", value: "lime" },
+						{ label: "Green", value: "green" },
+						{ label: "Emerald", value: "emerald" },
+						{ label: "Teal", value: "teal" },
+						{ label: "Cyan", value: "cyan" },
+						{ label: "Sky", value: "sky" },
+						{ label: "Blue", value: "blue" },
+						{ label: "Purple", value: "purple" },
+						{ label: "Indigo", value: "indigo" },
+						{ label: "Violet", value: "violet" },
+						{ label: "Fuchsia", value: "fuchsia" },
+						{ label: "Pink", value: "pink" },
+						{ label: "Rose", value: "rose" }
+					],
+					defaultValue: "violet",
+					required: true,
+				}, {
+					name: "shade",
+					type: "select",
+					options: [
+						{ label: "100", value: "100" },
+						{ label: "200", value: "200" },
+						{ label: "300", value: "300" },
+						{ label: "400", value: "400" },
+						{ label: "500", value: "500" },
+						{ label: "600", value: "600" },
+						{ label: "700", value: "700" },
+						{ label: "800", value: "800" },
+						{ label: "900", value: "900" },
+						{ label: "950", value: "950" },
+					],
+					defaultValue: "500",
+					required: true,
+				}
+			],
+		},
 	],
 
 };
