a233a9d52562cf43cf5acfaac92cdb242fb906b7
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-27 18:05:28 +0100

    Remove unused ATTENDEE_CATEGORIES constant in config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,41 +6,4 @@ export const GROUP_STATUSES = [
   { label: "Archived", value: "archived" },
 ] as const;
 
-export const ATTENDEE_CATEGORIES = [
-	{
-		label: "New Born",
-		value: "new_born",
-		description: "Children under 1 month old",
-	},
-	{
-		label: "Infant",
-		value: "infant",
-		description: "Children under 2 years old",
-	},
-	{
-		label: "Toddler",
-		value: "toddler",
-		description: "Children between 2 and 4 years old",
-	},
-  {
-    label: "Preschooler",
-    value: "preschooler",
-    description: "Children between 4 and 6 years old",
-  },
-  {
-    label: "School Age",
-    value: "school_age",
-    description: "Children between 6 and 12 years old",
-  
-  },
-	{
-		label: "Teen",
-		value: "teen",
-		description: "Children between 12 and 18 years old",
-	},
-  {
-    label: "Expectant Parent",
-    value: "expectant_parent",
-    description: "Parents expecting a child",
-  },
-] as const;
+
