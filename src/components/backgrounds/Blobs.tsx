f4c4a1202b8923b9ccd10c7b45ab3a3e8d103a93
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-30 14:52:59 +0100

    Update BackgroundBlobs component styles in Blobs.tsx

--- src/components/backgrounds/Blobs.tsx
+++ src/components/backgrounds/Blobs.tsx
@@ -7,7 +7,7 @@ type BackgroundBlobsProps = {
 export const BackgroundBlobs = ({ children }: BackgroundBlobsProps) => {
 
   return (
-    <div className="relative w-full max-w-lg -z-50">
+    <div className="relative -z-50 object-cover overflow-auto">
       <Blob className="top-14 left-16 bg-purple-300 animate-blob" />
       <Blob className="top-24 left-64 bg-yellow-300 animate-blob animation-delay-2000" />
       <Blob className="top-44 left-36 bg-pink-300 animate-blob animation-delay-4000" />
