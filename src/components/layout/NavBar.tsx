a8e295b52e4fa36991f8befd1b624a2ebabab07a
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-04-30 14:53:30 +0100

    Update NavBar component with NextLink and Button imports

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,17 +1,23 @@
+import { Button } from "../ui/button";
 import Container from "./Container";
+import NextLink from "next/link";
 
 export default function NavBar() {
 	return (
 		<div className="bg-white sticky z-50 t-0 inset-x-0 h-16 ">
 			<header className="relative bg-white border-b border-gray-100">
 				<Container>
-          <div className="">
+					<div className="">
 
-					<div className="flex items-center justify-between h-16">
-            {/* TODO Mobile Nav */}
-test
-        </div>
-          </div>
+						<div className="flex items-center justify-between h-16">
+							
+								{/* TODO Mobile Nav */}
+								<NextLink href={"/"} className="cursor-pointer px-0">
+									Group Finder
+								</NextLink>
+						
+						</div>
+					</div>
 				</Container>
 			</header>
 		</div>
