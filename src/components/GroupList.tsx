36ec6ecb2a610a53092ec1140e3b00487c64c2a1
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-05-14 17:47:29 +0100

    Refactor GroupList component to include user filtering

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AttendeeCategories } from "./AttendeeCategories";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import type { Group } from "@/cms/payload-types";
@@ -12,32 +14,43 @@ type GroupListProps = {
 }
 
 export function GroupList({ groups }: GroupListProps) {
+	
+
+	const userFilteredGroups = groups.filter(group => group)
+
 	return (
 
 		<>
 			<ul className="grid gap-2">
-				{groups.length === 0 && <li>No groups found</li>}
 
-				{groups.map(group => <li key={group.name}>
-					<Card >
-						<CardHeader>
-							<CardTitle className="flex flex-row gap-2 whitespace-nowrap">
-								<span className="text-xl align-middle">{group.name}</span>
-								{(group.status === "active") && <BiBadgeCheck className="text-primary align-middle text-2xl" />}
-							</CardTitle>
-							<AttendeeCategories attendeeCategories={group.attendeeCategories} />
-							<CardDescription>
-								{group.description}
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="">
-							<p>{group.description}</p>
-							<Schedule weeklySchedule={group.weeklySchedule} />
-							<ContactDetails contactDetails={group.contactDetails}></ContactDetails>
-							<GroupVenue venue={group.location} />
-						</CardContent>
-					</Card>
-				</li>)}
+				{groups.length === 0 && <li>No groups found</li>}
+				{groups.length > 0
+					&& userFilteredGroups.length === 0
+					&& <li>No groups matching filters</li>
+				}
+
+				{userFilteredGroups.map(group => {
+					return (<li key={group.name}>
+						<Card >
+							<CardHeader>
+								<CardTitle className="flex flex-row gap-2 whitespace-nowrap">
+									<span className="text-xl align-middle">{group.name}</span>
+									{(group.status === "active") && <BiBadgeCheck className="text-primary align-middle text-2xl" />}
+								</CardTitle>
+								<AttendeeCategories attendeeCategories={group.attendeeCategories} />
+								<CardDescription>
+									{group.description}
+								</CardDescription>
+							</CardHeader>
+							<CardContent className="">
+								<p>{group.description}</p>
+								<Schedule weeklySchedule={group.weeklySchedule} />
+								<ContactDetails contactDetails={group.contactDetails}></ContactDetails>
+								<GroupVenue venue={group.location} />
+							</CardContent>
+						</Card>
+					</li>)
+				})}
 			</ul>
 		</>
 	);
