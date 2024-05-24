819226bb45fb9eaf934cf1029d6296108587e508
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-05-24 17:57:13 +0100

    chore: Update npm dependencies for React Query and Babel plugin

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  experimental: {
+    reactCompiler: {
+      compilationMode: 'all',
+    },
+  },
 }
 
 export default withPayload(nextConfig)
