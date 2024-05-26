6c1187a934182aad111bed3579c86c49510f246a
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-05-26 21:06:50 +0100

    Refactor Loading component in GroupFinder page to use consistent indentation and update spinner styles

--- src/app/(group-finder)/loading.tsx
+++ src/app/(group-finder)/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
 	return (
-		<div className="flex items-center justify-center h-screen">
-			<div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900">
+		<div className="flex justify-center items-center h-screen">
+			<div className="border-gray-900 border-t-2 border-b-2 rounded-full w-32 h-32 animate-spin">
 				<span className="sr-only">Loading...</span>
 			</div>
 		</div>
