877f8b7b9d3baaee7e695ec20679e5695d496c9e
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-07-12 18:02:41 +0100

    refactor: Update formatTime function to use toLocaleTimeString for consistent time formatting

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,17 +1,17 @@
-"use client"
-
-// Needs to be a client function because of hydration errors when using Intl.DateTimeFormat on the server
-
 // utility function to format iso date string to human readable time string
 
 export const formatTime = (isoDate: string) => {
-  const date = new Date(isoDate);
+	const date = new Date(isoDate);
 
-  const formattedTime = Intl.DateTimeFormat('en-GB', {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-  }).format(date);
+	const locales: Intl.LocalesArgument = ["en-GB"];
+	const options: Intl.DateTimeFormatOptions = {
+		hour: "numeric",
+		minute: "numeric",
+    timeZone: "GB",
+    hourCycle: "h12",
+	};
+
+	const formattedTime = date.toLocaleTimeString(locales, options);
 
   return formattedTime;
 };
