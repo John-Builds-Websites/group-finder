d0628b8798d9a0cd1d3c0a8ac7882472286bfc68
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-07-12 18:04:10 +0100

    refactor: Update ScheduleList component to improve readability and consistency

--- src/components/ScheduleList.tsx
+++ src/components/ScheduleList.tsx
@@ -1,37 +1,45 @@
-"use client"
+"use client";
 
-import { formatTime } from "@/lib/time";
 import type { Group } from "@/cms/payload-types";
+import { formatTime } from "@/lib/time";
 import { BiCalendarWeek } from "react-icons/bi";
+import Time from "./Time";
 
 type ScheduleProps = {
 	weeklySchedule: Group["weeklySchedule"];
 };
 export function ScheduleList({ weeklySchedule }: ScheduleProps) {
-	return (<div className="bg-slate-100 rounded p-2 ">
-		<span className="flex flex-row gap-2">
-			<BiCalendarWeek className="text-xl"/>
-			
-			<h3>Schedule</h3>
-		</span>
+	return (
+		<div className="bg-slate-100 p-2 rounded">
+			<span className="flex flex-row gap-2">
+				<BiCalendarWeek className="text-xl" />
 
-		{weeklySchedule?.map(({
-			schedule
-		}) => {
-			if (!schedule) return;
-			if (typeof schedule === "string") return;
-			if (!schedule.weekdays) return;
-			return <ul key={schedule.id}>
+				<h3>Schedule</h3>
+			</span>
 
-				{schedule.weekdays.map(weekday => {
-					return (
-						<li key={weekday} className="py-1 flex flex-row">
-							<span className="capitalize font-bold w-16">{weekday}:</span> {formatTime(schedule.startTime)} - {formatTime(schedule.endTime)}
-						</li>
-					);
-				})}
-
-			</ul>;
-		})}
-	</div>);
+			{weeklySchedule?.map(({ schedule }) => {
+				if (!schedule) return;
+				if (typeof schedule === "string") return;
+				if (!schedule.weekdays) return;
+				return (
+					<ul key={schedule.id}>
+						{schedule.weekdays.map((weekday) => {
+							return (
+								<li
+									key={weekday}
+									suppressHydrationWarning={false}
+									className="flex flex-row py-1"
+								>
+									<span className="w-16 font-bold capitalize">{weekday}:</span>
+									<Time isoDate={schedule.startTime} />
+									<span className="px-2">-</span>
+									<Time isoDate={schedule.endTime} />
+								</li>
+							);
+						})}
+					</ul>
+				);
+			})}
+		</div>
+	);
 }
