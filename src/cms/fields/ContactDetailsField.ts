dafa77925406fb4346e50d9bfd5d9d2f48869e69
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-07-18 18:44:05 +0100

    refactor: Import ContactDetailsField from correct location in Groups.tsx

--- src/cms/fields/ContactDetailsField.ts
+++ src/cms/fields/ContactDetailsField.ts
@@ -1,62 +1,70 @@
 import type { Field } from "payload";
 
 export const ContactDetailsField: Field = {
-  name: "contactDetails",
-  admin: {
-    description: "Contact details for the group",
-  },
-  type: "array",
-  fields: [
-    {
-      type: "row",
-      fields: [
-        {
-          name: "type",
-          type: "select",
-          admin: {
-            width: "20%",
-          },
-          defaultValue: "url",
-          options: [
-            { label: "URL", value: "url" },
-            { label: "Email", value: "email" },
-            { label: "Phone", value: "phone" },
-          ],
-          required: true,
-        },
-        {
-          name: "value",
-          type: "text",
-          required: true,
-          hooks: {
-            beforeValidate: [
-              ({siblingData}) => {
-                if (siblingData?.type === "email" && !siblingData.value.includes("@")) {
-                  return "Email addresses must contain an @ symbol";
-                }
-                
-                if (siblingData?.type === "phone" && !siblingData.value.match(/^\+?\d+$/)) {
-                  return "Phone numbers must contain only numbers and an optional + at the start";
-                }
+	name: "contactDetails",
+	admin: {
+		description: "Contact details for the group",
+	},
+	type: "array",
+	fields: [
+		{
+			type: "row",
+			fields: [
+				{
+					name: "type",
+					type: "select",
+					admin: {
+						width: "20%",
+					},
+					defaultValue: "url",
+					options: [
+						{ label: "URL", value: "url" },
+						{ label: "Email", value: "email" },
+						{ label: "Phone", value: "phone" },
+					],
+					required: true,
+				},
+				{
+					name: "value",
+					type: "text",
+					required: true,
+					hooks: {
+						beforeValidate: [
+							({ siblingData }) => {
+								if (
+									siblingData?.type === "email" &&
+									!siblingData.value.includes("@")
+								) {
+									return "Email addresses must contain an @ symbol";
+								}
 
-                if (siblingData?.type === "url" && !siblingData.value.match(/^(https?:\/\/)/)) {
-                  return "URLs must start with http:// or https://";
-                }
+								if (
+									siblingData?.type === "phone" &&
+									!siblingData.value.match(/^\+?\d+$/)
+								) {
+									return "Phone numbers must contain only numbers and an optional + at the start";
+								}
 
-                return
-              }
-            ],
-          },
+								if (
+									siblingData?.type === "url" &&
+									!siblingData.value.match(/^(https?:\/\/)/)
+								) {
+									return "URLs must start with http:// or https://";
+								}
 
-        },
-        {
-          name: "customLabel",
-          type: "text",
-          admin: {
-            width: "20%",
-          },
-        },
-      ]
-    },
-  ],
-}
+								return;
+							},
+						],
+					},
+				},
+				{
+					name: "customLabel",
+					type: "text",
+					admin: {
+						width: "20%",
+					},
+				},
+			],
+		},
+	],
+};
