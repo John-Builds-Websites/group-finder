05aedd5ca8c7a4c56d20f7a5b238d1fe43ef9fee
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-07-18 18:44:00 +0100

    refactor: Update CustomIDField.ts to use consistent formatting and import paths

--- src/cms/fields/CustomIDField.ts
+++ src/cms/fields/CustomIDField.ts
@@ -2,18 +2,18 @@ import { nanoid } from "nanoid";
 import type { Field } from "payload";
 
 export const CustomIDField: Field = {
-  name: "id",
-  type: "text",
-  admin: {
-    disabled: true,
-  },
-  hooks: {
-    beforeValidate: [
-      ({ value }) => {
-        if (!value || value === "") {
-          return nanoid();
-        }
-      },
-    ],
-  },
-}
+	name: "id",
+	type: "text",
+	admin: {
+		disabled: true,
+	},
+	hooks: {
+		beforeValidate: [
+			({ value }) => {
+				if (!value || value === "") {
+					return nanoid();
+				}
+			},
+		],
+	},
+};
